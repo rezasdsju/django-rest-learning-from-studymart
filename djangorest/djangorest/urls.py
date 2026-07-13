0f6b9ebacef986c30127dd8a747fb25ed34fd687
rezasdsju <rezaul2002104@gmail.com>
2026-07-13 16:40:24 +0600

    CRUP operation using ModelMixin

--- djangorest/djangorest/urls.py
+++ djangorest/djangorest/urls.py
@@ -30,11 +30,12 @@ urlpatterns = [
     # path('aicreate/<int:pk>', views.aiquest_create, name='aicreate'),
     # path('aicreate/', views.AiquestCreate.as_view(), name='aicreate'),
     # path('aicreate/<int:pk>', views.AiquestCreate.as_view(), name='aicreate'),
-    path('ailist/', views.AiquestList.as_view(), name='ailist'),
-    path('aicreate/', views.AiquestCreate.as_view(), name='aicreate'),
-    path('airetrieve/', views.AiquestRetrieve.as_view()),
-    path('airetrieve/<int:pk>', views.AiquestRetrieve.as_view()),
-    path('aiupdate/<int:pk>', views.AiquestUpdate.as_view()),
-    path('aidestroy/<int:pk>', views.AiquestDestroy.as_view()),
+    # path('ailist/', views.AiquestList.as_view(), name='ailist'),
+    # path('aicreate/', views.AiquestCreate.as_view(), name='aicreate'),
+    path('ailistcreate/', views.Aiquest_List_Create.as_view()),
+    #path('airetrieve/', views.AiquestRetrieve.as_view()),
+    path('airetupdes/<int:pk>', views.Aiquest_Retrieve_Update_Destroy.as_view()),
+    # path('aiupdate/<int:pk>', views.AiquestUpdate.as_view()),
+    # path('aidestroy/<int:pk>', views.AiquestDestroy.as_view()),
     
 ]
--- djangorest/drapi/views.py
+++ djangorest/drapi/views.py
@@ -234,38 +234,26 @@ from . serializers import AiquestSerializer
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 
-class AiquestList(GenericAPIView, ListModelMixin):
+class Aiquest_List_Create(GenericAPIView, ListModelMixin, CreateModelMixin):
     queryset = Aiquest.objects.all()
     serializer_class = AiquestSerializer
     
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
     
-class AiquestCreate(GenericAPIView, CreateModelMixin):
-    queryset = Aiquest.objects.all()
-    serializer_class = AiquestSerializer
-    
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
     
-class AiquestRetrieve(GenericAPIView, RetrieveModelMixin):
+class Aiquest_Retrieve_Update_Destroy(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
     queryset = Aiquest.objects.all()
     serializer_class = AiquestSerializer
     
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
-    
 
-class AiquestUpdate(GenericAPIView, UpdateModelMixin):
-    queryset = Aiquest.objects.all()
-    serializer_class = AiquestSerializer
-    
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
-    
-class AiquestDestroy(GenericAPIView, DestroyModelMixin):
-    queryset = Aiquest.objects.all()
-    serializer_class = AiquestSerializer
+
     
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
