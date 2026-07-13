84bd2ca4cb2a33cc982d90672b9b8629e10bd8fd
rezasdsju <rezaul2002104@gmail.com>
2026-07-13 18:34:05 +0600

    Session Authentication

--- djangorest/djangorest/settings.py
+++ djangorest/djangorest/settings.py
@@ -123,8 +123,8 @@ STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        #'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
