9acbc5f1291c872312a4095cc674893446b7a4aa
rezasdsju <rezaul2002104@gmail.com>
2026-07-13 07:43:44 +0600

    first commit

---
+++ djangorest/drapi/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from . models import Aiquest
+
+# Register your models here.
+
+"""class AiquestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'teacher_name', 'course_name', 'course_duration', 'seat']
+admin.site.register(Aiquest, AiquestAdmin)"""
+
+@admin.register(Aiquest)
+class AiquestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'teacher_name', 'course_name', 'course_duration', 'seat']
