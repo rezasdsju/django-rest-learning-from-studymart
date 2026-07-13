d9efee1bbd103763f89d57f0dd4f05a08188678f
rezasdsju <rezaul2002104@gmail.com>
2026-07-13 07:45:13 +0600

    Complex Data has been created by models.py

--- djangorest/drapi/admin.py
+++ djangorest/drapi/admin.py
@@ -7,6 +7,7 @@ from . models import Aiquest
     list_display = ['id', 'teacher_name', 'course_name', 'course_duration', 'seat']
 admin.site.register(Aiquest, AiquestAdmin)"""
 
+
 @admin.register(Aiquest)
 class AiquestAdmin(admin.ModelAdmin):
     list_display = ['id', 'teacher_name', 'course_name', 'course_duration', 'seat']
